9e9139716432a01df6676f70dad9bf87d7bd5c9d
Uday <uday.pandey@gmail.com>
2019-12-02 14:18:14 +0000

    Added a placeholder login project template

---
+++ SimpleLogin/SimpleLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleLogin
+//
+//  Created by Uday Pandey on 02/12/2019.
+//  Copyright © 2019 Thirstysea Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
